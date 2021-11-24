ccb05ad59f53cbe3977fd10070bbf7b87f4051d0
sabby9 <88092742+sabby9@users.noreply.github.com>
2021-11-24 20:09:42 +0530

    Create kern.c

---
+++ kern.c
@@ -0,0 +1,18 @@
+#include<unistd.h>
+#include<sys/syscall.h>
+#include<stdio.h>
+#include<linux/kernel.h>
+
+#define sys_kernel_2d_memcpy 335
+
+int main(){
+  int row = 3,column = 3;
+  float orignalmatrix[row][column] = {{1,1,1},{2,2,2},{3,3,3}};
+  float copiedmatrix[row][column];
+  long int x = syscall(sys_kernel_2d_memcpy,*orignalmatrix,*copiedmatrix,row,column);
+  printf("%s \n \n","Copied Matrix-");
+  for(int i = 0 ; i < 3 ; i++){for(int j = 0 ; j < 3 ; j++){printf(" %f ",copiedmatrix[i][j]);}preintf("\n");}
+  return 0;
+}
+
+
